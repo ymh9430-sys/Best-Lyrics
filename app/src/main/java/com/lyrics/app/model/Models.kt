a69814462e64814a40e57150c5a00be6f05ee458
ymh9430-sys <ymh9430@gmail.com>
2026-04-30 13:58:12 +0300

    Create Models.kt

---
+++ app/src/main/java/com/lyrics/app/model/Models.kt
@@ -0,0 +1,21 @@
+package com.lyrics.app.model
+
+data class SongInfo(
+    val title: String,
+    val artist: String,
+    val album: String,
+    val duration: Int
+)
+
+data class LyricsResult(
+    val source: String,
+    val lyrics: String
+)
+
+sealed class UiState {
+    object Idle : UiState()
+    object Loading : UiState()
+    data class SongFound(val song: SongInfo) : UiState()
+    data class Success(val song: SongInfo, val results: List<LyricsResult>) : UiState()
+    data class Error(val message: String) : UiState()
+}
